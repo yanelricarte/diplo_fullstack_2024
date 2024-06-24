d512770c154d7bdc247a44cd0056822087361222
yricarte <ryanel@indracompany.com>
2024-06-24 18:00:46 -0300

    clase 17 aplicación js

--- clase17/js/main.js
+++ clase17/js/main.js
@@ -20,8 +20,6 @@ console.log(nombreMascota);
 
 
 //Funcion para agregar mascota a la lista 
-
-
 function agregarMascota(){
   const inputMascota = document.getElementById("nuevaMascota");
   const nombreMascota = inputMascota.value.trim();
@@ -31,8 +29,20 @@ function agregarMascota(){
     //Crear el elemento de la lista
     const elementoLista = document.createElement("li");
     elementoLista.textContent = nuevaMascota.nombre;
-        // Agregar clase si la mascota está adoptada
+    
+    // Botón para eliminar 
+    const botonEliminar = document.createElement("span");
+    botonEliminar.textContent = " Eliminar";
+    botonEliminar.classList.add("eliminar");
 
+    //Evento para eliminar 
+    botonEliminar.addEventListener("click", function(){
+      elementoLista.remove();
+    })
+
+
+
+        // Agregar clase si la mascota está adoptada
     if (nuevaMascota.adoptada){
       elementoLista.classList.add("adoptada")
     }
@@ -42,7 +52,14 @@ function agregarMascota(){
       nuevaMascota.adoptar();
       elementoLista.classList.add("adoptada")
 
-    })
+    });
+
+    //Evitar que el evento de adoptada se dispare al hacer click en eliminar
+    botonEliminar.addEventListener("click", function(event) {
+      event.stopPropagation();
+    });
+
+    elementoLista.appendChild(botonEliminar);
     const listaMascota = document.getElementById("listaMascotas"); 
     listaMascota.appendChild(elementoLista)
 
@@ -52,4 +69,12 @@ function agregarMascota(){
 
 
 const botonAgregarMascotas = document.getElementById("agregarMascota");
-botonAgregarMascotas.addEventListener("click", agregarMascota)
+botonAgregarMascotas.addEventListener("click", agregarMascota);
+
+//permitir agregar mascota al presionar enter
+document.getElementById("nuevaMascota").addEventListener("keyup", function(event) {
+  if(event.key === "Enter"){
+    agregarMascota();
+  }
+  
+})
